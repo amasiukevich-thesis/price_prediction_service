eeb0dcb54f771b3d6f1ab788fe2132498b0af38b
amasiukevich <anton.masiukiewicz1@gmail.com>
2023-07-30 17:52:44 +0200

    Second commit message

---
+++ ddl_scripts/init.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS rates (
+	unix BIGINT NOT NULL PRIMARY KEY,
+	rate_date TIMESTAMP NOT NULL,
+	symbol VARCHAR(10) NOT NULL,
+	price_open DECIMAL(20, 2) NOT NULL,
+	price_close DECIMAL(20, 2) NOT NULL,
+	price_high DECIMAL(20, 2) NOT NULL,
+	price_low DECIMAL(20, 2) NOT NULL,
+	volume_crypto DECIMAL(20, 2) NOT NULL,
+	volume_usd DECIMAL(20, 2) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS prediction_rates (
+	unix BIGINT NOT NULL PRIMARY KEY,
+	rate_date TIMESTAMP NOT NULL,
+	symbol VARCHAR(10) NOT NULL,
+	price_open DECIMAL(20, 2) NOT NULL,
+	price_close DECIMAL(20, 2) NOT NULL,
+	price_high DECIMAL(20, 2) NOT NULL,
+	price_low DECIMAL(20, 2) NOT NULL,
+	volume_crypto DECIMAL(20, 2) NOT NULL,
+	volume_usd DECIMAL(20, 2) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS subscriptions (
+	id SERIAL NOT NULL PRIMARY KEY,
+	user_id VARCHAR(255) NOT NULL,
+	CRYPTO VARCHAR(255) NOT NULL
+);
